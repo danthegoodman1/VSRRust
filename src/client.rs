dc0608578dcca39bc9862fc540358bf03817d8b6
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-10-26 09:25:12 -0700

    start working on send_request operation

--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,8 @@
-use crate::node::NodeIdentifier;
+use crate::{node::NodeIdentifier, rpc::{Reply, Request, RPC}};
 
 pub struct Client {
     pub id: usize,
-    pub last_request_number: usize,
+    pub next_request_number: usize,
     pub nodes: Vec<NodeIdentifier>,
     pub leader_index: usize,
 }
@@ -11,7 +11,7 @@ impl Client {
     pub fn new(id: usize, leader_index: usize) -> Self {
         Self {
             id,
-            last_request_number: 0,
+            next_request_number: 0,
             nodes: Vec::new(),
             leader_index,
         }
@@ -23,5 +23,17 @@ impl Client {
 
     pub fn set_leader_index(&mut self, leader_index: usize) {
         self.leader_index = leader_index;
+    }
+
+    pub fn send_request(&mut self, payload: Vec<u8>) -> Reply {
+        let rpc = RPC::Request(Request {
+            payload,
+            client_id: self.id,
+            request_number: self.next_request_number,
+        });
+        // TODO: send rpc using the transport protocol
+        // when we get a reply, we need to update the next request number
+        self.next_request_number += 1;
+        panic!("Not implemented");
     }
 }
