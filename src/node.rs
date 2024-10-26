6a983b7df57a608744e772f447dd28c0b9cb9518
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-10-25 19:11:32 -0700

    format

--- src/node.rs
+++ src/node.rs
@@ -1,12 +1,11 @@
 pub struct Node {
-  // Must be unique to the node
-  pub id: usize,
-  
+    // Must be unique to the node
+    pub id: usize,
 }
 
 pub struct NodeIdentifier {
-  pub id: usize,
-  pub address: String,
+    pub id: usize,
+    pub address: String,
 }
 
 use std::{collections::HashMap, fmt};
@@ -31,21 +30,21 @@ impl fmt::Display for NodeStatus {
 }
 
 struct ClientTableEntry {
-  /// The request number of the last request sent to the client
-  pub request_number: usize,
-  /// The last request sent to the client, if it was executed
-  pub last_request: Option<Request>,
+    /// The request number of the last request sent to the client
+    pub request_number: usize,
+    /// The last request sent to the client, if it was executed
+    pub last_request: Option<Request>,
 }
 
 pub struct NodeState {
-  /// Sorted array of nodes
-  pub nodes: Vec<NodeIdentifier>,
-  /// The replica number of this node (index in the nodes array)
-  pub replica_number: usize,
-  pub view_number: usize,
-  pub status: NodeStatus,
-  pub op_number: usize,
-  
-  log: Vec<Request>,
-  client_table: HashMap<usize, ClientTableEntry>,
+    /// Sorted array of nodes
+    pub nodes: Vec<NodeIdentifier>,
+    /// The replica number of this node (index in the nodes array)
+    pub replica_number: usize,
+    pub view_number: usize,
+    pub status: NodeStatus,
+    pub op_number: usize,
+
+    log: Vec<Request>,
+    client_table: HashMap<usize, ClientTableEntry>,
 }
--- src/rpc.rs
+++ src/rpc.rs
@@ -1,68 +1,67 @@
-
 pub struct Request {
-  /// The operation and its arguments
-  pub payload: Vec<u8>,
-  /// The client id
-  pub client_id: usize,
-  /// The request number for the client
-  pub request_number: usize,
+    /// The operation and its arguments
+    pub payload: Vec<u8>,
+    /// The client id
+    pub client_id: usize,
+    /// The request number for the client
+    pub request_number: usize,
 }
 
 pub struct Prepare {
-  pub view_number: usize,
-  pub payload: Vec<u8>,
-  pub op_number: usize,
-  pub commit_number: usize,
+    pub view_number: usize,
+    pub payload: Vec<u8>,
+    pub op_number: usize,
+    pub commit_number: usize,
 }
 
 pub struct PrepareOk {
-  pub view_number: usize,
-  pub op_number: usize,
-  pub node_id: usize,
+    pub view_number: usize,
+    pub op_number: usize,
+    pub node_id: usize,
 }
 
 pub struct Reply {
-  pub view_number: usize,
-  pub request_number: usize,
-  pub payload: Vec<u8>,
+    pub view_number: usize,
+    pub request_number: usize,
+    pub payload: Vec<u8>,
 }
 
 pub struct Commit {
-  pub view_number: usize,
-  pub commit_number: usize,
+    pub view_number: usize,
+    pub commit_number: usize,
 }
 
 pub struct StartViewChange {
-  pub new_view_number: usize,
-  pub node_id: usize,
+    pub new_view_number: usize,
+    pub node_id: usize,
 }
 
 pub struct DoViewChange {
-  pub new_view_number: usize,
-  pub log: Vec<Request>,
-  pub last_normal_view_number: usize,
-  pub op_number: usize,
-  pub commit_number: usize,
-  pub node_id: usize,
+    pub new_view_number: usize,
+    pub log: Vec<Request>,
+    pub last_normal_view_number: usize,
+    pub op_number: usize,
+    pub commit_number: usize,
+    pub node_id: usize,
 }
 
 pub struct StartView {
-  pub new_view_number: usize,
-  pub log: Vec<Request>,
-  pub op_number: usize,
-  pub commit_number: usize,
+    pub new_view_number: usize,
+    pub log: Vec<Request>,
+    pub op_number: usize,
+    pub commit_number: usize,
 }
 
 pub struct Recovery {
-  pub node_id: usize,
-  pub nonce: usize,
+    pub node_id: usize,
+    pub nonce: usize,
 }
 
 pub struct RecoveryResponse {
-  pub view_number: usize,
-  pub nonce: usize,
-  pub log: Vec<Request>,
-  pub op_number: usize,
-  pub commit_number: usize,
-  pub view_primary_node_id: usize,
+    pub view_number: usize,
+    pub nonce: usize,
+    pub log: Vec<Request>,
+    pub op_number: usize,
+    pub commit_number: usize,
+    pub view_primary_node_id: usize,
 }
